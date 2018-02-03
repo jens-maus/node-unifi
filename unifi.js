99173f6d1d15eea3fa85ebb91db28d46c7dbfc7b
Jacob Alberty <jacob.alberty@gmail.com>
2018-02-03 01:55:41 -0600

    Support PUT requests

--- unifi.js
+++ unifi.js
@@ -1373,7 +1373,10 @@ var Controller = function(hostname, port)
         // on the json data supplied and the overriding method
         if(json !== null)
         {
-          reqfunc = request.post;
+          if(method === 'PUT')
+            reqfunc = request.put;
+          else
+            reqfunc = request.post;
           reqjson.json = json;
         }
         else if(typeof(method) === 'undefined')
