7a9e4f1e508914fa778dd08dc6ac8d1d8b0bc9ba
Jens Maus <mail@jens-maus.de>
2020-05-20 10:18:01 +0200

    splitted all tests into separate it() iterations and added all other tests we had before. Still WIP to add all kind of API functions found in node-unifi, but we are on our way...

--- test/01-login-logout.js
+++ test/01-login-logout.js
@@ -22,42 +22,148 @@ if (process.env.CONTROLLER_PASS) {
 
 // Run the tests
 describe('Running tests', () => {
-  it('login/logout', done => {
-    const controller = new unifi.Controller(CONTROLLER_IP, CONTROLLER_PORT);
+  const controller = new unifi.Controller(CONTROLLER_IP, CONTROLLER_PORT);
+
+  // LOGIN
+  it('login()', done => {
     if (controller !== null) {
       controller.login(CONTROLLER_USER, CONTROLLER_PASS, err => {
         if (err) {
           done(err);
         } else {
-          // GET SITE STATS
-          controller.getSitesStats((err, sites) => {
-            if (typeof (sites) === 'undefined' || sites.length <= 0) {
-              done('ERROR: getSitesStats()');
-            }
-            if (err) {
-              done(err);
-            } else {
-              // GET CLIENT DEVICES
-              controller.getClientDevices(sites[0].name, (err, client_data) => {
-                if (typeof (client_data) === 'undefined' || client_data.length < 0) {
-                  done('ERROR: getClientDevices()');
-                }
-                if (err) {
-                  done(err);
-                } else {
-                  controller.logout(err => {
-                    if (err) {
-                      done(err);
-                    } else {
-                      done();
-                    }
-                  });
-                }
-              });
-            }
-          });
+          done();
         }
       });
     }
+  });
+
+  // GET SITE STATS
+  let controller_sites = null;
+  it('getSitesStats()', done => {
+    controller.getSitesStats((err, sites) => {
+      if (err) {
+        done(err);
+      } else if (typeof (sites) === 'undefined' || sites.length <= 0) {
+        done('ERROR: getSitesStats()');
+      } else {
+        controller_sites = sites;
+        done();
+      }
+    });
+  });
+
+  // GET SITE SYSINFO
+  it('getSiteSysinfo()', done => {
+    controller.getSiteSysinfo(controller_sites[0].name, (err, sysinfo) => {
+      if (err) {
+        done(err);
+      } else if (typeof (sysinfo) === 'undefined' || sysinfo.length <= 0) {
+        done('ERROR: getSiteSysinfo()');
+      } else {
+        done();
+      }
+    });
+  });
+
+  // GET CLIENT DEVICES
+  it('getClientDevices()', done => {
+    controller.getClientDevices(controller_sites[0].name, (err, client_data) => {
+      if (err) {
+        done(err);
+      } else if (typeof (client_data) === 'undefined' || client_data.length < 0) {
+        done('ERROR: getClientDevices()');
+      } else {
+        done();
+      }
+    });
+  });
+
+  // GET ALL USERS EVER CONNECTED
+  it('getAllUsers()', done => {
+    controller.getAllUsers(controller_sites[0].name, (err, users_data) => {
+      if (err) {
+        done(err);
+      } else if (typeof (users_data) === 'undefined' || users_data.length < 0) {
+        done('ERROR: getAllUsers()');
+      } else {
+        done();
+      }
+    });
+  });
+
+  // GET ALL ACCESS DEVICES
+  it('getAccessDevices()', done => {
+    controller.getAccessDevices(controller_sites[0].name, (err, access_data) => {
+      if (err) {
+        done(err);
+      } else if (typeof (access_data) === 'undefined' || access_data.length < 0) {
+        done('ERROR: getAccessDevices()');
+      } else {
+        done();
+      }
+    });
+  });
+
+  // GET ALL SESSIONS
+  it('getSessions()', done => {
+    controller.getSessions(controller_sites[0].name, (err, session_data) => {
+      if (err) {
+        done(err);
+      } else if (typeof (session_data) === 'undefined' || session_data.length < 0) {
+        done('ERROR: getSessions()');
+      } else {
+        done();
+      }
+    });
+  });
+
+  // GET ALL AUTHORIZATIONS
+  it('getAllAuthorizations()', done => {
+    controller.getAllAuthorizations(controller_sites[0].name, (err, auth_data) => {
+      if (err) {
+        done(err);
+      } else if (typeof (auth_data) === 'undefined' || auth_data.length < 0) {
+        done('ERROR: getSessions()');
+      } else {
+        done();
+      }
+    });
+  });
+
+  // GET USERS
+  it('getUsers()', done => {
+    controller.getAllAuthorizations(controller_sites[0].name, (err, user_data) => {
+      if (err) {
+        done(err);
+      } else if (typeof (user_data) === 'undefined' || user_data.length < 0) {
+        done('ERROR: getUsers()');
+      } else {
+        done();
+      }
+    });
+  });
+
+  // GET SELF
+  it('getSelf()', done => {
+    controller.getSelf(controller_sites[0].name, (err, self_data) => {
+      if (err) {
+        done(err);
+      } else if (typeof (self_data) === 'undefined' || self_data.length < 0) {
+        done('ERROR: getSelf()');
+      } else {
+        done();
+      }
+    });
+  });
+
+  // LOGOUT
+  it('logout()', done => {
+    controller.logout(err => {
+      if (err) {
+        done(err);
+      } else {
+        done();
+      }
+    });
   });
 });
