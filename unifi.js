5eac98dbe70414a0fbdb21129c4fcd1380ea1438
Simon Christmann <simon@christmann.email>
2023-09-27 23:01:05 +0200

    allow to supply urlParams for WebSocket conenction

--- unifi.js
+++ unifi.js
@@ -2927,13 +2927,19 @@ class Controller extends EventEmitter {
 
   /**
    * WebSocket listen function
+   * 
+   * @param  object       $options        Supply additional options for WebSocket. Currently one implemented option is to add urlParams to the WebSocket URL, e.g.: `{urlParams: {clients: 'v2'}}`.
    */
-  async listen() {
+  async listen(options) {
     const cookies = await this._cookieJar.getCookieString(this._baseurl.href);
 
     let eventsUrl = new URL(`wss://${this._baseurl.host}/wss/s/${this.opts.site}/events`);
     if (this._unifios) {
       eventsUrl = new URL(`wss://${this._baseurl.host}/proxy/network/wss/s/${this.opts.site}/events`);
+    }
+
+    for (const [key, value] of Object.entries(options?.urlParams ?? {})) {
+      eventsUrl.searchParams.set(key, value);
     }
 
     this._ws = new WebSocket(eventsUrl.href, {
